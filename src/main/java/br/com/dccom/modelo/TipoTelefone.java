525b4d5619c20918a397b560f9797c5529a82eae
Daniel Santos <drsantos@gmail.com>
2014-10-24 02:56:54 -0200

    inclusao de entidade: tipo telefone

---
+++ src/main/java/br/com/dccom/modelo/TipoTelefone.java
@@ -0,0 +1,50 @@
+package br.com.dccom.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class TipoTelefone {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="tipoTelefone_id")
+	private int id;
+	
+	private String descricao;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "telefone_id", nullable = true)
+	private Telefone telefone = new Telefone();
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Telefone getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+}
